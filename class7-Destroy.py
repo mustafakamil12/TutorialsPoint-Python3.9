d4933473b41cd4b39f21323555a4d894e1bf05f1
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Air.local>
2021-03-04 12:34:45 -0600

    update class scrpits

--- class7-Destroy.py
+++ class7-Destroy.py
@@ -1,14 +1,8 @@
 #!/Library/Frameworks/Python.framework/Versions/3.9/bin/python3.9
 
-class Point:
-   def __init__( self, x=0, y=0):
-      self.x = x
-      self.y = y
-   def __del__(self):
-      class_name = self.__class__.__name__
-      print (class_name, "destroyed")
+import point
 
-pt1 = Point()
+pt1 = point.Point()
 pt2 = pt1
 pt3 = pt1
 print (" pt1 id: ",id(pt1),"\n", "pt2 id: ", id(pt2), "\n", "pt3 id: ", id(pt3))   # prints the ids of the obejcts
---
+++ class8.py
@@ -0,0 +1,36 @@
+#!/Library/Frameworks/Python.framework/Versions/3.9/bin/python3
+
+class Parent:        # define parent class
+   parentAttr = 100
+   def __init__(self):
+      print ("Calling parent constructor")
+
+   def parentMethod(self):
+      print ('Calling parent method')
+
+   def setAttr(self, attr):
+      Parent.parentAttr = attr
+
+   def getAttr(self):
+      print ("Parent attribute :", Parent.parentAttr)
+   
+   def myMethod(self):
+       print ("myMethod in parent class...")
+
+class Child(Parent): # define child class
+   def __init__(self):
+      print ("Calling child constructor")
+
+   def childMethod(self):
+      print ('Calling child method')
+   
+   def myMethod(self):                      # override myMethod ...
+       print ("myMethod in child class...")
+
+
+c = Child()          # instance of child
+c.childMethod()      # child calls its method
+c.parentMethod()     # calls parent's method
+c.setAttr(200)       # again call parent's method
+c.getAttr()          # again call parent's method
+c.myMethod()         # call myMethod to check if it's in parent or in child
