b8c3510cc5f78bcb6c3e0216455d6630ea08d218
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Air.local>
2021-04-13 09:18:43 -0500

    working on datetime_convert.py script

--- datetime_convert.py
+++ datetime_convert.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 date_time_str = '2018-06-29 08:15:27.243860'
 date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
@@ -6,3 +7,17 @@ date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f'
 print('Date:', date_time_obj.date())
 print('Time:', date_time_obj.time())
 print('Date-time:', date_time_obj)
+
+print("time.time() = ", time.gmtime(time.time()))
+
+print("time() = ", time.time())
+
+#datetime.fromtimestamp(timestamp)
+
+build_time = time.strftime('%Y-%m-%d %H:%M:%S',time.gmtime(time.time()))
+print(build_time)
+
+now = datetime.datetime.now()
+
+elapsed = "%02d:%02d" % (now.minute,now.second)
+print("elapsed: ", elapsed)
